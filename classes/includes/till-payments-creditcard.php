f54d30f7a8156ddebca2f3b441bca6670eda595a
leo-till <76463578+leo-till@users.noreply.github.com>
2021-12-10 11:13:39 +1100

    add in visa/mc logo next to credit card option

--- classes/includes/till-payments-creditcard.php
+++ classes/includes/till-payments-creditcard.php
@@ -25,7 +25,7 @@ class WC_TillPayments_CreditCard extends WC_Payment_Gateway
     {
         $this->id = TILL_PAYMENTS_EXTENSION_UID_PREFIX . $this->id;
         $this->method_description = TILL_PAYMENTS_EXTENSION_NAME . ' ' . $this->method_title . ' payments.';
-
+        $this->icon = 'https://s3.ap-southeast-2.amazonaws.com/images.simplepays.io/visa_mastercard+(2).png';
         $this->has_fields = isset($_GET['pay_for_order']);
 
         $this->init_form_fields();
@@ -388,7 +388,7 @@ class WC_TillPayments_CreditCard extends WC_Payment_Gateway
 
         echo '<script>window.integrationKey="' . $this->get_option('integrationKey') . '";</script><style>.payment_box iframe { width: 100%!important }</style>
         <div id="till_payments_errors"></div>
-        <div class="payment_box" style="padding: 25px; background-color: #fff; border-radius: 3px; width: 450px; min-height: 330px">
+        <div class="payment_box" style="padding: 25px; background-color: #fff; border-radius: 3px; max-width: 450px; min-height: 385px">
             <div id="till_payments_seamless">
                 <input type="hidden" id="till_payments_token" name="token">
                 <p class="form-row form-row-wide" style="height: 80px;">
